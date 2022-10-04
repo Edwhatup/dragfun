df07e46e804017af1d53cbc9b936b49609895ee5
Cubafehis0 <2656023557@qq.com>
2022-10-04 23:39:51 +0800

    添加ignore文件

---
+++ Assets/Resource/Scripts/CardDisplay.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CardDisplay : MonoBehaviour
+{
+    public Text nameText;
+    public Text attackText;
+    public Text healthText;
+    public Text effectText;
+
+    public Image backgroundImage;
+
+    public Card card;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowCard();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void  ShowCard()
+    {
+        nameText.text = card.cardName;
+        if(card is MonsterCard)
+        {
+            var monster = card as MonsterCard;
+            attackText.text = monster.attack.ToString();
+            healthText.text = monster.healthPoint.ToString();
+
+            
+            if (monster.effect == null)
+            {
+                effectText.gameObject.SetActive(false);
+            }
+            else
+            {
+                effectText.text = monster.effect;
+            }
+
+
+
+        }
+
+        else if(card is SpellCard)
+        {
+            var spell = card as SpellCard;
+            effectText.text = spell.effect;
+
+            attackText.gameObject.SetActive(false);
+            healthText.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Resource/Scripts/ClickCard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public enum CardState
+{
+    Library,Deck
+}
+
+public class ClickCard : MonoBehaviour,IPointerDownHandler
+{
+    // Start is called before the first frame update
+
+    private DeckManager DeckManager;
+    //private PlayerData PlayerData;
+
+    public CardState state;
+
+
+    void Start()
+    {
+        DeckManager = GameObject.Find("DataManager").GetComponent<DeckManager>();
+        //PlayerData = GameObject.Find("DataManager").GetComponent<PlayerData>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
+        int id = this.GetComponent<CardDisplay>().card.id;
+        
+        DeckManager.UpdateCard(state, id);
+    }
+}
