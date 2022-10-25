85401e5b767486537d222ecbea9d2d7eadcb1114
Edwhatup <58328515+Edwhatup@users.noreply.github.com>
2022-10-26 02:55:18 +0800

    新卡
    极限将军，哨兵和白骨巫师（后两个没做完）

---
+++ Assets/Scripts/Cards/Monster/ExtremeGeneral.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CardEvent;
+using Core;
+
+namespace Card.Monster
+{
+    
+    public class ExtremeGeneral : MonsterCard
+    {
+        int hpModifierOnForward;
+        int atkModifierOnForward;
+
+        public ExtremeGeneral(string name, int atk ,int hp,params string[] paras): base(name, atk, hp, paras)
+        {
+            int.TryParse(paras[0], out hpModifierOnForward);
+            int.TryParse(paras[1], out atkModifierOnForward);
+        }
+
+        [EventListener]
+        public void OnSummon(object o)
+        {
+            SummonMonsterEvent summonEvent = o as SummonMonsterEvent;
+            if(summonEvent != null && summonEvent.monster==this)
+            {
+                //Debug.Log("OnForward");
+                int onBoardCount= CardManager.Instance.board.Count;
+                for(int i=0; i<onBoardCount;i++)
+                {
+                    BattleManager.Buff(this, this, hpModifierOnForward, atkModifierOnForward);
+                }
+            }
+        }
+        public override string GetDesc()
+        {
+            return $"入场时，场上每有一个随从（包括自己），获得+{hpModifierOnForward}/+{atkModifierOnForward}";
+        }
+    }
+}
---
+++ Assets/Scripts/Cards/Monster/Sentinel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CardEvent;
+using Core;
+
+//没做完还
+namespace Card.Monster
+{
+    
+    public class Sentinel : MonsterCard
+    {
+        int dragCount;
+        public Sentinel(string name, int atk ,int hp,params string[] paras): base(name, atk, hp, paras)
+        {
+            int.TryParse(paras[0], out dragCount);
+        }
+
+        [EventListener]
+        public void OnAttack(object o)
+        {
+            
+        }
+        public override string GetDesc()
+        {
+            return $"每次攻击，抽{dragCount}张牌";
+        }
+    }
+}
