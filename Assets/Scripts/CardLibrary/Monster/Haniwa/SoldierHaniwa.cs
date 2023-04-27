6eb32f9272e0add0678193e082f6b25c4665d0ac
Edwhatup <58328515+Edwhatup@users.noreply.github.com>
2023-04-27 22:25:49 +0800

    埴轮新卡
    有bug

---
+++ Assets/Scripts/CardLibrary/Monster/Haniwa/SoldierHaniwa.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierHaniwa : Card
+{
+    public SoldierHaniwa(CardInfo info) : base(info)
+    {
+        name = "战士埴轮";
+        camp = CardCamp.Friendly;
+        type = CardType.Monster;
+        race = CardRace.Haniwa;
+        cost = 3;
+        AddComponnet(new AttackComponent(1));
+        AddComponnet(new AttackedComponent(4));
+        AddComponnet(new ActionComponent());
+        AddComponnet(new SummonComponent());
+        AddComponnet(new FieldComponnet());
+        var e=new HaniwaOnBoardListener(1,1);
+        AddComponnet(e);
+        GetDesc = () => e.ToString();
+    }
+}
---
+++ Assets/Scripts/Cards/EventListener/HaniwaOnBoardBuff.cs
@@ -0,0 +1,37 @@
+﻿public class HaniwaOnBoardListener : EventListenerComponent
+{
+    int hpMod;
+    int atkMod;
+    int boardAttackCount=0;
+    public HaniwaOnBoardListener(string[] args):base(null)
+    {
+        int.TryParse(args[0], out hpMod);
+        int.TryParse(args[1], out atkMod);
+    }
+    public HaniwaOnBoardListener(int hpMod, int atkMod):base(null)
+    {
+        this.hpMod = hpMod;
+        this.atkMod = atkMod;
+    }
+
+    public override string ToString()
+    {
+        if (hpMod == 0 && atkMod == 0) return "";
+        if (hpMod == 0) return $"场上每次入场埴轮随从时+{hpMod}攻击力";
+        if (atkMod == 0) return $"场上每次入场埴轮随从时+{hpMod}生命值";
+        return $"场上每次入场埴轮随从时，获得+{atkMod}+{hpMod}";
+    }
+
+    public override void EventListen(AbstractCardEvent e)
+    {
+        if (e is AfterSummonEvent & e.source.race==CardRace.Haniwa & e.source!=card)
+        {
+            var summon = e as AfterSummonEvent;
+            if(card.field.state==BattleState.Survive) 
+            {         
+                card.Buff(card,atkMod, hpMod); 
+            }
+        }
+    }
+
+}
