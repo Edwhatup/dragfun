3a1ba9552eebdaf97024464b764c7a8aacf2657b
Hamster5295 <hamster5295@163.com>
2023-05-02 00:16:47 +0800

    Debug

--- Assets/Scripts/Cards/SelectorEffect/NoTargetEffect/RandomDamage2Enemy.cs
+++ Assets/Scripts/Cards/SelectorEffect/NoTargetEffect/RandomDamage2Enemy.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class RandomDamage2Enemy : NoTargetCardEffect
 {
     int damage;
-    RangeType range=RangeType.AllEnemies;
-    int targetCnt=1;
+    RangeType range = RangeType.AllEnemies;
+    int targetCnt = 1;
 
     public RandomDamage2Enemy(Card card, string[] paras) : base(card)
     {
@@ -17,34 +17,35 @@ public class RandomDamage2Enemy : NoTargetCardEffect
         this.damage = damage;
     }
 
-    public RandomDamage2Enemy(Card card, int damage,RangeType range,int targetCnt) : base(card)
+    public RandomDamage2Enemy(Card card, int damage, RangeType range, int targetCnt) : base(card)
     {
-        this.range=range;
-        this.targetCnt=targetCnt;
+        this.range = range;
+        this.targetCnt = targetCnt;
         this.damage = damage;
     }
 
-    public override string ToString()   
+    public override string ToString()
     {
-        if(range==RangeType.AllEnemies && targetCnt==1)
-        {return $"随机对一名敌人造成{damage}点伤害";}
+        if (range == RangeType.AllEnemies && targetCnt == 1)
+        { return $"随机对一名敌人造成{damage}点伤害"; }
         else
-        return $"随机对{CardManager.Instance.GetSpecificAreaName(range)}的{targetCnt}名敌人造成{damage}点伤害";
+            return $"随机对{CardManager.Instance.GetSpecificAreaName(range)}的{targetCnt}名敌人造成{damage}点伤害";
     }
 
     public override void Excute()
     {
         //Debug.Log("excute start");
-        if(range==RangeType.AllEnemies && targetCnt==1)
+        if (range == RangeType.AllEnemies && targetCnt == 1)
         {
-            var enemy = CardManager.Instance.Enemies.FindAll(e=>e.field.state==BattleState.Survive).GetRandomItem();
+            var enemy = CardManager.Instance.Enemies.FindAll(e => e.field.state == BattleState.Survive).GetRandomItem();
             enemy.attacked.ApplyDamage(card, damage);
         }
         else
         {
-            var enemy =CardManager.Instance.GetSpecificAreaEnemies(card,card,range).GetRandomItems(targetCnt);
-            foreach(var a in enemy)
+            var enemy = CardManager.Instance.GetSpecificAreaEnemies(card, card, range).GetRandomItems(targetCnt);
+            foreach (var a in enemy)
             {
+                // Debug.Log($"{card.name} 随机攻击了 {a.name}");
                 a.attacked.ApplyDamage(card, damage);
             }
         }
