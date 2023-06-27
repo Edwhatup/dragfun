55f0fb9c26951e9f358770200ff7326e7dd862e7
Edwhatup <58328515+Edwhatup@users.noreply.github.com>
2023-06-27 13:05:40 +0800

    新卡
    偶像造物

---
+++ Assets/Scripts/CardLibrary/Monster/Haniwa/IdolCreature.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdolCreature : Card
+{
+    public IdolCreature(CardInfo info) : base(info)
+    {
+        name = "偶像造物";
+        camp = CardCamp.Friendly;
+        type = CardType.FriendlyDerive;
+        race = CardRace.Haniwa;
+        cost = 0;
+        AddComponnet(new AttackComponent(1));
+        AddComponnet(new AttackedComponent(1));
+        AddComponnet(new ActionComponent());
+        AddComponnet(new FieldComponnet());
+        //没做完
+
+        GetDesc = () => "";
+
+    }
+}
