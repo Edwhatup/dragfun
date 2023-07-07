c3822928a53807039d00e4205cac6db825504816
Edwhatup <58328515+Edwhatup@users.noreply.github.com>
2023-07-07 16:30:43 +0800

    新敌人

---
+++ Assets/Scripts/CardEffect/Effect/HaloEffect/YellElfHalo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellElfHalo : HaloEffect
+{
+    protected override List<Cell> Cells
+        => CellManager.Instance.GetCells().FindAll(c => CellManager.Instance.GetStreetDistance(c, card.field.cell) !=0);
+    private int extraDamage;
+    CardBuff buff;
+
+    public YellElfHalo(Card c, int extraDamage) : base(c)
+    {
+        this.extraDamage = extraDamage;
+    }
+
+    public override void InitTarget()
+    {
+        CardTargets.Add(CardTarget.Enemy);
+    }
+
+    public override void Execute(Card c)
+    {
+        buff =new StatsPositiveBuff(0,0,extraDamage);
+        c.AddBuff(buff);
+    }
+
+    public override void Undo(Card c)
+    {
+        c.RemoveBuff(buff);
+    }
+
+    public override string ToString()
+    {
+        return $"全场的友方造成的任意伤害都增加{extraDamage}";
+    }
+}
---
+++ Assets/Scripts/CardLibrary/Enemy/Elves.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShelterElf : Card
+{
+    public ShelterElf(CardInfo Info) : base(Info)
+    {
+        name = "庇护妖精";
+        type = CardType.EnemyDerive;
+        camp = CardCamp.Enemy;
+        AddComponent(new AttackedComponent(5){ taunt = 1 });
+        AddComponent(new FieldComponnet());
+        AddComponent(new EnemyAction());
+        AddComponent(new EnemyEffectListener(6, new EnemyNormalAttack(this, 3,1)) { priority = 0});
+        enemyAction.GetNextAction();
+        GetDesc = () => enemyAction.current?.ToString() ?? "";
+    }
+
+}
+
+public class YellElf : Card
+{
+    public YellElf(CardInfo Info) : base(Info)
+    {
+        name = "应援妖精";
+        type = CardType.EnemyDerive;
+        camp = CardCamp.Enemy;
+        AddComponent(new AttackedComponent(3));
+        AddComponent(new FieldComponnet());
+        AddComponent(new EnemyAction());
+        AddComponent(new HaloComponent(new YellElfHalo(this,1)));
+        AddComponent(new EnemyEffectListener(5, new EnemyNormalAttack(this, 3,1)) { priority = 0});
+        enemyAction.GetNextAction();
+        GetDesc = () => enemyAction.current?.ToString() ?? "";
+    }
+
+}
+
+public class WeakElf : Card
+{
+    public WeakElf(CardInfo Info) : base(Info)
+    {
+        name = "虚弱妖精";
+        type = CardType.EnemyDerive;
+        camp = CardCamp.Enemy;
+        AddComponent(new AttackedComponent(3));
+        AddComponent(new FieldComponnet());
+        AddComponent(new EnemyAction());
+        AddComponent(new HaloComponent(new YellElfHalo(this,1)));// 还没改完
+        AddComponent(new EnemyEffectListener(5, new EnemyNormalAttack(this, 3,1)) { priority = 0});
+        enemyAction.GetNextAction();
+        GetDesc = () => enemyAction.current?.ToString() ?? "";
+    }
+
+}
