97ecedf229fa540692bc3fc8ed7dca420f57e4ad
Hamster5295 <hamster5295@163.com>
2023-07-16 12:08:38 +0800

    修复了报错

--- Assets/Scripts/CardEffect/Buff/StatsPositiveBuff.cs
+++ Assets/Scripts/CardEffect/Buff/StatsPositiveBuff.cs
@@ -6,44 +6,46 @@ public class StatsPositiveBuff : CardBuff
     private int hp = 0;
     private int extraDamage = 0;
 
-    public StatsPositiveBuff(int atk,int hp ) : base("身材加成", -1, BuffType.Positive, BuffLifeType.Board)
+    public StatsPositiveBuff(int atk, int hp) : base("身材加成", -1, BuffType.Positive, BuffLifeType.Board)
     {
-        if(atk < 0 ) Debug.LogError("错误: 攻击加成<0!");
+        if (atk < 0) Debug.LogError("错误: 攻击加成<0!");
         this.atk = atk;
-        this.hp=hp;
+        this.hp = hp;
     }
-    
-    public StatsPositiveBuff(int atk,int hp,int extraDamage) : base("身材加成", -1, BuffType.Positive, BuffLifeType.Board)
+
+    public StatsPositiveBuff(int atk, int hp, int extraDamage) : base("身材加成", -1, BuffType.Positive, BuffLifeType.Board)
     {
-        if(atk < 0 ) Debug.LogError("错误: 攻击加成<0!");
+        if (atk < 0) Debug.LogError("错误: 攻击加成<0!");
         this.atk = atk;
-        this.hp=hp;
-        this.extraDamage=extraDamage;
+        this.hp = hp;
+        this.extraDamage = extraDamage;
     }
 
     public override void Execute()
     {
         if (card.attack != null)
+        {
             card.attack.atk += atk;
-            card.attacked.maxHp+=hp;
-            card.attacked.hp+=hp;
+            card.attacked.maxHp += hp;
+            card.attacked.hp += hp;
             card.attack.extraDamage += extraDamage;
+        }
     }
 
     public override void Undo()
     {
         if (card.attack != null)
             card.attack.atk -= atk;
-            card.attacked.maxHp+=hp;
-            card.attacked.hp-=hp;            
-            card.attack.extraDamage -= extraDamage;
+        card.attacked.maxHp += hp;
+        card.attacked.hp -= hp;
+        card.attack.extraDamage -= extraDamage;
 
 
     }
 
-    public override string GetDesc() 
+    public override string GetDesc()
     {
-        if(extraDamage!=0) return $"额外造成{extraDamage}点伤害, 剩余{lifeTimer}";
+        if (extraDamage != 0) return $"额外造成{extraDamage}点伤害, 剩余{lifeTimer}";
         else return $"获得+{atk}/+{hp}, 剩余{lifeTimer}";
-    } 
+    }
 }
