d9955872cbfc3664d0d8ed253cefc545c488d33f
Edwhatup <58328515+Edwhatup@users.noreply.github.com>
2023-07-20 10:44:38 +0800

    新卡

---
+++ Assets/Scripts/CardEffect/Effect/EnemyEffect/BigElfSummon.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigElfSummon : NoTargetCardEffect
+{
+    int damage;
+    int count;
+    public BigElfSummon(Card card, int damage, int count) : base(card)
+    {
+        this.damage = damage;
+        this.count = count;
+    }
+    public override string ToString()
+    {
+        return $"进行{damage}x{count}次攻击";
+    }
+    public override void Excute()
+    {
+        if(card.GetComponent<DirectAtkCountdownComponent>().Ready!=true)
+        {
+            NoFace();
+        }
+        else Face();
+
+    }
+
+    private void NoFace()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            var targets = CellManager.Instance.GetCells()
+                    .FindAll(c => c.row == 0 &&
+                                    c.card != null &&
+                                    c.card.camp == CardCamp.Friendly &&
+                                    c.card.attacked != null &&
+                                    c.card.field.state == BattleState.Survive);
+            if (targets.Count > 0)
+            {
+                targets[0].card.attacked.ApplyDamage(card, damage);
+            }
+            else
+                Player.Instance.attacked.ApplyDamage(card, damage);
+        }
+    }
+
+    private void Face()
+    {
+        for (int i = 0; i < count-1; i++)
+        {
+            var targets = CellManager.Instance.GetCells()
+                    .FindAll(c => c.row == 0 &&
+                                    c.card != null &&
+                                    c.card.camp == CardCamp.Friendly &&
+                                    c.card.attacked != null &&
+                                    c.card.field.state == BattleState.Survive);
+            if (targets.Count > 0)
+            {
+                targets[0].card.attacked.ApplyDamage(card, damage);
+            }
+            else
+                Player.Instance.attacked.ApplyDamage(card, damage);
+        }
+        Player.Instance.attacked.ApplyDamage(card, damage);
+    }
+}
--- Assets/Scripts/CardEffect/Effect/EnemyEffect/RandomSummonDerive.cs
+++ Assets/Scripts/CardEffect/Effect/EnemyEffect/RandomSummonDerive.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class RandomSummonDerive : NoTargetCardEffect
 {
-    string deriveName;
+    List<string> deriveNames=null;
+    string deriveName=null;
     int count;
     public RandomSummonDerive(Card card, string name, int count) : base(card)
     {
         this.deriveName = name;
+        this.count = count;
+    }
+    public RandomSummonDerive(Card card, List<string> names, int count) : base(card)
+    {
+        this.deriveNames = names;
         this.count = count;
     }
     public override string ToString()
@@ -19,15 +25,26 @@ public class RandomSummonDerive : NoTargetCardEffect
     {
         return CellManager.Instance.GetCells().FindAll(c => c.CanSummon()).Count > 0;
     }
-    public override void Excute()
+    public void SingleSummon(string name, int count)
     {
         for (int i = 0; i < count; i++)
         {
             var cell = CellManager.Instance.GetCells().FindAll(c => c.CanSummon()).GetRandomItem();
-            var info = new CardInfo() { name = deriveName };
+            var info = new CardInfo() { name = name };
             var card = CardStore.Instance.CreateCard(info);
             card.field.Summon(cell);
             // CardManager.Instance.drawDeck.Transfer(CardManager.Instance.board, card);
+        }
+    }
+    public override void Excute()
+    {
+        if(deriveName!=null)
+        {
+            SingleSummon(deriveName,count);
+        }
+        else
+        {
+            
         }
     }
 }
