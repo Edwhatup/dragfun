da665a89530b8277f19e5a363ce8c64c12b04b12
Edwhatup <58328515+Edwhatup@users.noreply.github.com>
2023-07-23 17:45:39 +0800

    TestCard

---
+++ Assets/Scripts/CardLibrary/Spell/TestSpell.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSpell : Card
+{
+    public TestSpell(CardInfo info) : base(info)
+    {
+        name = "用于关卡测试";
+        camp = CardCamp.Friendly;
+        type = CardType.Spell;
+        cost = 0;
+        var e = new GroupDamage(this,100,RangeType.AllEnemies);
+        AddComponent(new SpellCastComponent(1, e));
+        GetDesc=()=>e.ToString();    
+    }
+}
