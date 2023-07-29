4d674c424911897690ca4aa607e6f55be19935f0
Edwhatup <58328515+Edwhatup@users.noreply.github.com>
2023-07-29 14:46:04 +0800

    新卡

---
+++ Assets/Scripts/CardEffect/Effect/CellEffect/Constant/StatsDownCellHalo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsDownCellHalo : ConstantCellEffect
+{
+    protected override List<Cell> Cells
+        => CellManager.Instance.GetCells().FindAll(c => CellManager.Instance.GetStreetDistance(c, card.field.cell) == 0);
+    protected override int LifeTime => -1;
+
+    private int amount = 1;
+    Cell cell;
+
+    public StatsDownCellHalo (Card card,Cell cell,int amount) : base(card) 
+    {
+        this.cell=cell;
+        this.amount=amount;
+    }
+
+    public override void Execute(Card c)
+    {
+        c.attack.atk -= amount;
+    }
+
+    public override void Undo(Card c)
+    {
+        c.attacked.block += amount;
+    }
+
+    public override string ToString()
+    {
+        return $"降低其上随从{amount}点攻击力";
+    }
+
+    public override void InitTarget()
+    {
+        TargetCount = 1;
+        CardTargets.Add(CardTarget.Monster);
+    }
+}
--- Assets/Scripts/CardLibrary/Enemy/Ghost.cs
+++ Assets/Scripts/CardLibrary/Enemy/Ghost.cs
@@ -2,6 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+public class Komachi : Card
+{
+    public Komachi(CardInfo Info) : base(Info)
+    {
+        name = "小野塚小町";
+        type = CardType.Enemy;
+        camp = CardCamp.Enemy;
+        AddComponent(new AttackedComponent(5){});
+        AddComponent(new FieldComponnet());
+        AddComponent(new EnemyAction());
+        AddComponent(new DirectAtkCountdownComponent(5));
+        AddComponent(new EnemyEffectListener(7, new RandomSummonDerive(this, "怨灵", 2)) { priority = 1 });
+        AddComponent(new EnemyEffectListener(6, new EnemyNormalAttack(this, 3,1)) { priority = 0});
+        enemyAction.GetNextAction();
+        GetDesc = () => enemyAction.current?.ToString() ?? "";
+    }
+}   
+
 public class Ghost : Card
 {
     public Ghost(CardInfo Info) : base(Info)
@@ -9,13 +29,13 @@ public class Ghost : Card
         name = "怨灵";
         type = CardType.EnemyDerive;
         camp = CardCamp.Enemy;
-        AddComponent(new AttackedComponent(5){ taunt = 1 });
+        AddComponent(new AttackedComponent(5){});
         AddComponent(new FieldComponnet());
         AddComponent(new EnemyAction());
+        AddComponent(new DeadComponent(new StatsDownCellHalo(this,this.field.cell,1)));
         AddComponent(new EnemyEffectListener(6, new EnemyNormalAttack(this, 3,1)) { priority = 0});
         enemyAction.GetNextAction();
         GetDesc = () => enemyAction.current?.ToString() ?? "";
     }
-    //没做完
 }   
 
