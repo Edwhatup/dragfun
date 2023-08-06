a0776477a35ec0f7bf99893cbc979528c4e042c8
Edwhatup <58328515+Edwhatup@users.noreply.github.com>
2023-08-06 16:49:14 +0800

    卡牌和新增的卡牌效果

---
+++ Assets/Scripts/CardEffect/Effect/EnemyEffect/PlayerDamage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : NoTargetCardEffect
+{
+    int damage;
+    public PlayerDamage(Card card,int damage) : base(card)
+    {
+        this.damage = damage;
+    }
+    public override string ToString()
+    {
+        return $"对你造成{damage}点伤害";
+    }
+    public override void Excute()
+    {
+        Player.Instance.attacked.ApplyDamage(card, damage);
+    }
+}
--- Assets/Scripts/CardLibrary/Enemy/Ghost.cs
+++ Assets/Scripts/CardLibrary/Enemy/Ghost.cs
@@ -11,10 +11,10 @@ public class Komachi : Card
         name = "小野塚小町";
         type = CardType.Enemy;
         camp = CardCamp.Enemy;
-        AddComponent(new AttackedComponent(5){});
+        AddComponent(new AttackedComponent(40){});
         AddComponent(new FieldComponnet());
         AddComponent(new EnemyAction());
-        AddComponent(new DirectAtkCountdownComponent(5));
+        AddComponent(new DirectAtkCountdownComponent(20));
         AddComponent(new EnemyEffectListener(7, new RandomSummonDerive(this, "怨灵", 2)) { priority = 1 });
         AddComponent(new EnemyEffectListener(6, new EnemyNormalAttack(this, 3,1)) { priority = 0});
         enemyAction.GetNextAction();
@@ -37,5 +37,25 @@ public class Ghost : Card
         enemyAction.GetNextAction();
         GetDesc = () => enemyAction.current?.ToString() ?? "";
     }
-}   
+}
+
+public class EvilSpirit : Card
+{
+    public EvilSpirit(CardInfo Info) : base(Info)
+    {
+        name = "恶灵";
+        type = CardType.EnemyDerive;
+        camp = CardCamp.Enemy;
+        AddComponent(new AttackedComponent(5){});
+        AddComponent(new FieldComponnet());
+        AddComponent(new EnemyAction());
+        AddComponent(new DeadComponent(new PlayerDamage(this,3)));
+        AddComponent(new EnemyEffectListener(6, new EnemyNormalAttack(this, 3,1)) { priority = 0});
+        enemyAction.GetNextAction();
+        GetDesc = () => enemyAction.current?.ToString() ?? "";
+    }
+}
+
+
+
 
