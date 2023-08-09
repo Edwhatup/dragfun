b1aa1fb714af408228c3ab5ef68b86dbd0957f38
Hamster5295 <hamster5295@163.com>
2023-08-09 20:28:23 +0800

    修个bug

---
+++ Assets/Scripts/UI/UITriggers.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class UITriggers : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public UnityEvent onClick;
+
+    private float timer;
+    private bool holding = false;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        timer = 0;
+        holding = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (timer < 0.1f) onClick.Invoke();
+    }
+
+    private void Update()
+    {
+        if (holding)
+            timer += Time.deltaTime;
+    }
+}
